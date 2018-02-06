b64e445bf98ca5c706dd0f8a60fe609317ed95d5
Aish97Ram06 <shiva.aishwarya97@gmail.com>
2018-02-06 12:50:44 +0530

    Initial commit

---
+++ app/src/main/java/com/example/aishwarya/recycle2/Contact.java
@@ -0,0 +1,15 @@
+package com.example.aishwarya.recycle2;
+
+/**
+ * Created by aishwarya on 5/2/18.
+ */
+
+public class Contact {
+
+    public String id;
+    public String name;
+    public String email;
+    public String address;
+    public String gender;
+
+}
